198fd3f17dbb5778b6fd26a9cbfc2dea7f626284
Elif T. Kus <elifkus@gmail.com>
2015-04-17 20:08:06 +0300

    Added test cases

--- src/arraysandstrings/string_with_unique_characters_test.go
+++ src/arraysandstrings/string_with_unique_characters_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestContainsOnlyUniqueChars(t *testing.T) {
 	cases := []struct {
-		in string, want bool
+		in string; want bool
 	}{
 		{"Hello, world", false},
 		{"Hello, 世界", false},
@@ -15,7 +15,7 @@ func TestContainsOnlyUniqueChars(t *testing.T) {
 	for _, c := range cases {
 		got := ContainsOnlyUniqueChars(c.in)
 		if got != c.want {
-			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("Reverse(%q) == %t, want %t", c.in, got, c.want)
 		}
 	}
 }
