4735c2bad4481ec2919a748886043e0b0b24e9b0
Elif T. Kuş <elifkus@gmail.com>
2017-01-11 11:11:23 +0200

    Added new method to find random question within given chapter

--- src/numbers/random_question_finder.go
+++ src/numbers/random_question_finder.go
@@ -41,3 +41,19 @@ func FindQuestion() (int, int) {
 	return chapter, question
 }
 
+func FindQuestionInChapter(chapter int) int {
+	numberOfQuestionsByChapters := [17]int{9,8,6,12,8,10,12,14,8,11,6,11,8,7,7,26,26}
+	
+	if (chapter < 1 || chapter > 17) {
+		return -1;
+	}
+	
+	rand.Seed(time.Now().UnixNano())
+	
+	question := rand.Intn(numberOfQuestionsByChapters[chapter-1]) + 1
+	
+	fmt.Printf("%d.%d\n", chapter,  question)
+
+	return question
+} 
+
--- src/numbers/random_question_finder_test.go
+++ src/numbers/random_question_finder_test.go
@@ -17,7 +17,20 @@ func TestFindQuestion(t *testing.T)  {
 	
 	if question < 1 || question > numberOfQuestionsByChapters[chapter-1] {
 		t.Errorf("Question number for chapter %d cannot be larger than %d, or smaller than 1\nQuestion number is %d", 
-						chapter, numberOfQuestionsByChapters[chapter], question)
+						chapter, numberOfQuestionsByChapters[chapter-1], question)
+	}
+}
+
+func TestFindQuestionInChapter(t *testing.T)  {
+	numberOfQuestionsByChapters := [17]int{9,8,6,12,8,10,12,14,8,11,6,11,8,7,7,26,26}
+	
+	chapter := 17
+	
+	question := FindQuestionInChapter(chapter)
+	
+	if question < 1 || question > numberOfQuestionsByChapters[chapter-1] {
+		t.Errorf("Question number for chapter %d cannot be larger than %d, or smaller than 1\nQuestion number is %d", 
+						chapter, numberOfQuestionsByChapters[chapter-1], question)
 	}
 }
 
