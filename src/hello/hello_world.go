c8d0f92908049a5ce92d02ede3fd1b7f1740a47e
Elif T. Kuş <elifkus@gmail.com>
2017-02-14 11:10:05 +0200

    Implemented swap

--- src/hello/hello_world.go
+++ src/hello/hello_world.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 )
 
+func swap(x *int, y *int) {
+	*x, *y = *y, *x
+}
+
 func main() {
 	fmt.Println("Hello world!")
+	
+	var xPointer = new(int)
+	var yPointer = new(int)
+	*xPointer = 3
+	*yPointer = 4
+	
+	swap(xPointer, yPointer)
+	
+	fmt.Printf("x: %d y: %d", *xPointer, *yPointer)
+	
 }
 
